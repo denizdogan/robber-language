5088419a9061f39bb7a386282e7abbde5874d6d9
Deniz Dogan <deniz@dogan.se>
2016-01-29 20:59:17 +0100

    Add more test cases and clean up code

--- test/test.js
+++ test/test.js
@@ -3,34 +3,42 @@ require('core-js/fn/object/entries');
 import assert from 'assert';
 import { encode, decode } from '../src/index';
 
-var ENTRIES = Object.entries({
+const ENTRIES = Object.entries({
+  ' \t\n': ' \t\n',
   'lorem ipsum': 'lolororemom ipopsosumom',
-  'dolor sit amet': 'dodololoror sositot amometot'
+  'dolor sit amet': 'dodololoror sositot amometot',
+  'aoeuiy': 'aoeuiy',
+  'Badabing Bada Boom': 'BoBadodabobinongog BoBadoda BoBoomom',
+  'bcdfghjklmnpqrstvwxz':
+    'bobcocdodfofgoghohjojkoklolmomnonpopqoqrorsostotvovwowxoxzoz',
+  'BCDFGHJKLMNPQRSTVWXZ':
+    'BoBCoCDoDFoFGoGHoHJoJKoKLoLMoMNoNPoPQoQRoRSoSToTVoVWoWXoXZoZ',
+  'No, thanks!': 'NoNo, tothohanonkoksos!'
 });
 
-describe('robber-language', function() {
-  it('should encode correctly', function() {
-    ENTRIES.forEach(function([input, output]) {
+describe('robber-language', () => {
+  it('should encode correctly', () => {
+    ENTRIES.forEach(([input, output]) => {
       assert.equal(encode(input), output);
     });
   });
 
-  it('should decode correctly', function() {
-    ENTRIES.forEach(function([input, output]) {
+  it('should decode correctly', () => {
+    ENTRIES.forEach(([input, output]) => {
       assert.equal(decode(output), input);
     });
   });
 
-  it('should return the input string after encoding then decoding', function() {
+  it('should return the input string after encoding then decoding', () => {
     var exec = input => decode(encode(input));
-    ENTRIES.forEach(function([input, _output]) {
+    ENTRIES.forEach(([input, _output]) => {
       assert.equal(exec(input), input);
     })
   });
 
-  it('should return the input string after decoding then encoding', function() {
+  it('should return the input string after decoding then encoding', () => {
     var exec = input => encode(decode(input));
-    ENTRIES.forEach(function([_input, output]) {
+    ENTRIES.forEach(([_input, output]) => {
       assert.equal(exec(output), output);
     });
   });
