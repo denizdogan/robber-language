568d1a954cbfd22de0c6f6cce77328c9f4aa2a23
Deniz Dogan <deniz@dogan.se>
2016-01-29 20:58:43 +0100

    Include 'w' as a consonant

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export function encode(input) {
-  const REGEXP = /([bcdfghjklmnpqrstvxz])/gi;
+  const REGEXP = /([bcdfghjklmnpqrstvwxz])/gi;
   return input.replace(REGEXP, '$1o$1')
 }
 
 export function decode(input) {
-  const REGEXP = /([bcdfghjklmnpqrstvxz])o\1/gi;
+  const REGEXP = /([bcdfghjklmnpqrstvwxz])o\1/gi;
   return input.replace(REGEXP, '$1')
 }
